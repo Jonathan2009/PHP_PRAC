fd06f49dca7f6436339b721d109a707f48546e2d
JR <ronquillojonathan1i@gmail.com>
2021-06-30 07:52:12 -0500

    metodo post

---
+++ monitoreo2.php
@@ -0,0 +1,9 @@
+<?php
+    $temperatura = $_POST['temp'];
+    $voltaje = $_POST['volt'];
+    $humedad = $_POST['hum'];
+    echo $temperatura."<br>";
+    echo $voltaje."<br>";
+    echo $humedad."<br>";
+
+?>
