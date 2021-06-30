a56a0fb828863b3f818262932329d4dfa34666f6
JR <ronquillojonathan1i@gmail.com>
2021-06-30 07:32:15 -0500

    Formulario

---
+++ monitoreo.php
@@ -0,0 +1,9 @@
+<?php
+    $temperatura = $_GET['temp'];
+    $voltaje = $_GET['volt'];
+    $humedad = $_GET['hum'];
+    echo $temperatura."<br>";
+    echo $voltaje."<br>";
+    echo $humedad."<br>";
+
+?>
